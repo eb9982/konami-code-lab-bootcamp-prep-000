5822fd2f2ac33147b9d5b2ca88dcbab4a36ab32a
Luke Borsare <lborsare1@gmail.com>
2017-09-21 22:44:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+<<<<<<< HEAD
   let i = 0
   function check(){
     if (i === code.length){
@@ -18,5 +19,22 @@ function init() {
          i  = 0
       }
     })
+=======
+  // Write your JavaScript code inside the init() function
+  var i = 0
+  var arr = []
+  var arrl = arr.length
+  if (window.addEventListener){
+    window.addEventListener("keydown", function(e) {
+      if (e.which == code[arr.length]){
+        i++
+      }
+      else {
+        var i  = 0
+      }
+      if (arr == code){
+        alert("Konami!")
+      }
+>>>>>>> 51ed732ee0ff41198d9b2510433f7e3fb5166c72
   }
 }
